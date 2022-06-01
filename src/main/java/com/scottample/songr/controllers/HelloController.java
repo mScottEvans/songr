df4dd050dc5d907198d1a1febfc8aef8e8c82829
Scott Evans <michealsevans20@gmail.com>
2022-06-01 18:26:35 -0500

    finished task 1

--- src/main/java/com/scottample/songr/controllers/HelloController.java
+++ src/main/java/com/scottample/songr/controllers/HelloController.java
@@ -1,4 +1,15 @@
 package com.scottample.songr.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class HelloController {
+    @GetMapping("hello")
+    public String HelloWorld() {
+        return "hello";
+    }
+
 }
