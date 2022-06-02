b9901da2eda46bd72264757643ebc3e0b5eb3f10
Scott Evans <michealsevans20@gmail.com>
2022-06-01 19:04:49 -0500

    add

--- src/main/java/com/scottample/songr/Album.java
+++ src/main/java/com/scottample/songr/Album.java
@@ -1,4 +1,18 @@
 package com.scottample.songr;
 
 public class Album {
+    String title;
+    String artist;
+    Integer songCount;
+    Integer length;
+    String imageUrl;
+
+    public Album(String title, String artist, Integer songCount, Integer length, String imageUrl){
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
 }
